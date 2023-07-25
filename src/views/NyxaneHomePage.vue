a3b35904c0686b448d783c5ce22a44e07d49928d
Gamedemons <72032280+Gamedemons@users.noreply.github.com>
2023-07-26 00:27:24 +0530

    Temporary : Slide menu button

--- src/views/NyxaneHomePage.vue
+++ src/views/NyxaneHomePage.vue
@@ -31,11 +31,6 @@ function changeTheme() {
   }
 }
 
-function menuIcon(e) {
-  e.target.classList.toggle('opened');
-  e.target.setAttribute('aria-expanded', this.classList.contains('opened'))
-}
-
 // Normal js here
 
 // js ends here
@@ -83,14 +78,7 @@ onMounted(() => {
         <img v-show="theme==='light'" id="logo-img" src="@/assets/Logo/nyxane_low_light.png" alt="Nyxane">
       </div>
       <div id="nav-menu">
-        <button>Demo</button>
-        <button id="nav-menu-button" class="menu" @click="menuIcon" aria-label="Main Menu">
-          <svg width="100" height="100" viewBox="0 0 100 100">
-            <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
-            <path class="line line2" d="M 20,50 H 80" />
-            <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
-          </svg>
-        </button>
+        <div class="btn from-right"><div id="fx-cut"></div>Menu</div>
       </div>
     </nav>
     <section id="planetary_system">
@@ -164,22 +152,8 @@ onMounted(() => {
 
 #nav-menu {
   display: grid;
-  grid-template-columns: 1fr 1fr;
   justify-self: right;
   align-self: center;
-}
-
-#nav-menu button{
-  margin: 0px 5px 0px 5px;
-}
-
-#nav-menu-button {
-  background: transparent;
-}
-
-#nav-menu-button svg {
-  height: 60px;
-  width: 60px;
 }
 
 #logo-img {
@@ -483,4 +457,78 @@ onMounted(() => {
   stroke-width: 6;
 }
 
+
+
+
+
+
+
+
+
+/* ~~~~~~~ INIT. BTN ~~~~~~~ */
+#fx-cut {
+  position: absolute;
+  height: 5.25em;
+  width: 2em;
+  top: 8px;
+  left: -17px;
+  background-color: black;
+  rotate: -45deg;
+
+}
+.btn {
+  position: relative;
+  padding: 1.25rem 1rem;
+  padding-left: 3em;
+  color: var(--inv);
+  letter-spacing: 0.4rem;
+  text-transform: uppercase;
+  transition: all 500ms cubic-bezier(0.77, 0, 0.175, 1);
+}
+
+.btn:before, .btn:after {
+  content: '';
+  position: absolute;
+  transition: inherit;
+  z-index: -1;
+}
+
+.btn:hover {
+  color: var(--def);
+  transition-delay: .5s;
+}
+
+.btn:hover:before {
+  transition-delay: 0s;
+}
+
+.btn:hover:after {
+  background: var(--inv);
+  transition-delay: .35s;
+}
+
+/* From Right */
+
+.from-right:before,
+.from-right:after {
+  top: 0;
+  width: 0;
+  height: 100%;
+}
+
+.from-right:before {
+  left: 0;
+  border: 1px solid var(--inv);
+  border-left: 0;
+  border-right: 0;
+}
+
+.from-right:after {
+  right: 0;
+}
+
+.from-right:hover:before,
+.from-right:hover:after {
+  width: 100%;
+}
 </style>
